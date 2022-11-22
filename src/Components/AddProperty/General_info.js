bc4b6854d4e354887920cd83992b266ef22dff7e
PAVAN BACHUWAR <117831833+PAVANBACHUWAR@users.noreply.github.com>
2022-11-22 17:36:43 +0530

    Add files via upload

---
+++ src/Components/AddProperty/General_info.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Location_info from './AddProperty/Location_info'
+
+export default function Generalinfo(){
+    return (
+        <div>
+            <Location_info/>
+            i am from Generalinfo
+        </div>
+    )
+}
