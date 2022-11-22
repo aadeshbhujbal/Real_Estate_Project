30328928d9e907f4746a08c8e19bdc60d58aaa1e
shiven.vrm@gmail.com <shiven.vrm@gmail.com>
2022-11-22 19:42:22 +0530

    implemented userschema

--- Server/models/signupSchema.js
+++ Server/models/signupSchema.js
@@ -1,11 +1,126 @@
 const mongoose = require("mongoose");
+
 const validator = require("validator");
 
-const signupSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
+  // BASIC INFO
+  //-----------------------------------------------
   _id: Number,
-  username: {
+  property_type: {
     type: String,
     required: true,
+  },
+  negotiable: {
+    type: String,
+    required: true,
+  },
+  ownership: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  property_age: {
+    type: String,
+    required: true,
+  },
+  property_approved: {
+    type: String,
+    required: true,
+  },
+  property_description: {
+    type: String,
+    required: true,
+  },
+  bank_loan: {
+    type: String,
+    required: true,
+  },
+  //PROPERTY DETAILS
+  //-----------------------------------
+  length: {
+    type: Number,
+    required: true,
+  },
+  breadth: {
+    type: Number,
+    required: true,
+  },
+  total_area: {
+    type: Number,
+    required: true,
+  },
+  area_unit: {
+    type: String,
+    required: true,
+  },
+  no_of_bhk: {
+    type: String,
+    required: true,
+  },
+  no_of_floors: {
+    type: Number,
+    required: true,
+  },
+  attached: {
+    type: String,
+    required: true,
+  },
+  western_toilet: {
+    type: String,
+    required: true,
+  },
+  furnished: {
+    type: String,
+    required: true,
+  },
+  car_parking: {
+    type: String,
+    required: true,
+  },
+  lift: {
+    type: String,
+    required: true,
+  },
+  electricity: {
+    type: String,
+    required: true,
+  },
+  facing: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    min: 2,
+  },
+  mobile: {
+    type: Number,
+    required: true,
+    min: 10,
+  },
+  posted_by: {
+    type: String,
+    required: true,
+  },
+  sale_type: {
+    type: String,
+    required: true,
+  },
+  featured_package: {
+    type: String,
+    required: true,
+  },
+  ppd_package: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    // required:true
   },
   email: {
     type: String,
@@ -16,41 +131,49 @@ const signupSchema = new mongoose.Schema({
       }
     },
   },
-  password: {
+  city: {
     type: String,
+    required: true,
+  },
+  area: {
+    type: String,
+    required: true,
+  },
+  pincode: {
+    type: Number,
     required: true,
     min: 6,
   },
-  cpassword: {
+  address: {
     type: String,
     required: true,
-    min: 6,
+  },
+  landmark: {
+    type: String,
+    required: true,
+  },
+  latitude: {
+    type: Number,
+    required: true,
+  },
+  longitude: {
+    type: Number,
+    required: true,
+  },
+  views: {
+    type: Number,
+    // default:random_views
+  },
+  status: {
+    type: String,
+    // default:random_string
+  },
+  days_left: {
+    type: Number,
+    // default:random_days
   },
 });
 
-signupSchema.pre("save", function (next) {
-  let doc = this;
-  sequencing
-    .getSequenceNextValue("user_id")
-    .then((counter) => {
-      console.log("asdasd", counter);
-      if (!counter) {
-        sequencing
-          .insertCounter("user_id")
-          .then((counter) => {
-            doc._id = counter;
-            console.log(doc);
-            next();
-          })
-          .catch((error) => next(error));
-      } else {
-        doc._id = counter;
-        next();
-      }
-    })
-    .catch((error) => next(error));
-});
-
-const signupModal = mongoose.model("signup", signupSchema);
-
-module.exports = signupModal;
+userSchema.pre("save");
+const userModel = new mongoose.model("user", userSchema);
+module.exports = userModel;
