acbbedd132cfdcaaaf604be6491cd23fe8bdaeed
vickybharath410 <vickybharath410@gmail.com>
2022-11-24 00:09:45 +0530

    config folder uploaded

--- Server/config/sequencingProperty.js
+++ Server/config/sequencingProperty.js
@@ -1,33 +1,3 @@
-const mongoose = require("mongoose");
-const SequenceSchema = new mongoose.Schema({
-  _id: {
-    type: String,
-    required: true,
-  },
-  seq: {
-    type: Number,
-    required: true,
-  },
-});
-const PropertyCounter = mongoose.model("PropertyCounter", SequenceSchema);
-const getSequenceNextValue = (seqName) => {
-  return new Promise((resolve, reject) => {
-    PropertyCounter.findByIdAndUpdate(
-      { _id: seqName },
-      { $inc: { seq: 1 } },
-      (error, counter) => {
-        if (error) {
-          reject(error);
-        }
-        if (counter) {
-          resolve(counter.seq + 1);
-        } else {
-          resolve(null);
-        }
-      }
-    );
-  });
-};
 const insertCounter = (seqName) => {
   const newCounter = new PropertyCounter({ _id: seqName, seq: 1000 });
   return new Promise((resolve, reject) => {
