4e9b3298e1e407c3086b9bccd53a094b775c0438
vickybharath410 <vickybharath410@gmail.com>
2022-11-24 00:17:01 +0530

    protected folder uploaded

--- Client/src/Components/Protected/protected.js
+++ Client/src/Components/Protected/protected.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Navigate } from "react-router-dom";
 import { Cookies } from "react-cookie";
 
@@ -11,3 +12,27 @@ const Protected = ({ children }) => {
   return <>{token.length ? children : <Navigate to="/login" />}</>;
 };
 export default Protected;
+=======
+
+import { Navigate } from "react-router-dom";
+import { Cookies } from 'react-cookie';
+// import Error404 from "../404 Error/Error404";
+
+const Protected = ({children})=> {
+    const cookies = new Cookies()
+    let token = cookies.get('jwt')
+    if(token === undefined){
+        token = ""
+    }
+
+    return (
+        <>
+       {token.length ? children: <Navigate to="/login" /> }
+
+        </>
+    )
+}
+export default Protected;
+
+
+>>>>>>> ea84db2e (protected folder uploaded)
